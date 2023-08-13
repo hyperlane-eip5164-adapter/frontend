eb389665bfff8ce41412af53f330391a2c7dee3c
degencodebeast <degencodebeast@protonmail.com>
2023-08-13 05:22:54 +0100

    wrote collectionCards component

--- app/components/CollectionCard.tsx
+++ app/components/CollectionCard.tsx
@@ -1,19 +1,67 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react"
+import { Box, Checkbox, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { useState,useEffect } from "react";
+import { useDispatch } from 'react-redux'
+import { addCollection, removeCollection } from "@/app/state/slices";
 
-const CollectionCard = () => {
+const CollectionCard = ({ collection }: { collection: any }) => {
+  const dispatch=useDispatch()
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheck = (collection: any) => {
+const _isChecked=isChecked;
+if(_isChecked){
+  dispatch(removeCollection(collection))
+    }
+    else{
+      dispatch(addCollection(collection))
+    }
+    setIsChecked(!_isChecked);
+  };
+// useEffect(()=>{
+
+// },[dispatch,isChecked])
+  
   return (
-    <Flex direction={'column'} p={3} boxShadow={'md'} borderRadius={'2xl'} bg={'white'}>
-<Box bg={'red.400'} h={200} borderRadius={'inherit'} w={250}>
-
-</Box>
-<Box mt={4}>
-    <Box>
-
-<Text as={'strong'}>Tin tin NFT</Text>
-    </Box>
-</Box>
+    <Flex
+      onClickCapture={() => handleCheck(collection)}
+      cursor={"pointer"}
+      border={"4px"}
+      borderColor={isChecked ? "brand.700" : "transparent"}
+      pos={"relative"}
+      direction={"column"}
+      p={3}
+      boxShadow={"md"}
+      borderRadius={"2xl"}
+      bg={"brand.900"}
+    >
+      <Checkbox
+        onChange={() => handleCheck(collection)}
+        isChecked={isChecked}
+        bg={"gray.700"}
+        size={"lg"}
+        pos={"absolute"}
+      />
+      <Box
+        overflow={"hidden"}
+        bg={"gray.400"}
+        h={200}
+        borderRadius={"inherit"}
+        w={250}
+      >
+        <Image
+          h={"full"}
+          w={"full"}
+          alt=""
+          src="/images/nft.jpg"
+          objectFit={"cover"}
+        />
+      </Box>
+      <Box mt={4}>
+        <Box>
+          <Text as={"strong"}>Tin tin NFT</Text>
+        </Box>
+      </Box>
     </Flex>
-  )
-}
+  );
+};
 
-export default CollectionCard
+export default CollectionCard;
--- app/components/CollectionCards.tsx
+++ app/components/CollectionCards.tsx
@@ -1,15 +1,22 @@
-import { Flex } from "@chakra-ui/react"
-import CollectionCard from "./CollectionCard"
+import { Box, Flex, Text } from "@chakra-ui/react";
+import CollectionCard from "./CollectionCard";
+import isEmpty from "just-is-empty";
 
-const CollectionCards = () => {
-    return (
-        <Flex minH={450} py={4} px={4} gap={'4'} wrap={'wrap'} >
-     <CollectionCard/>
-     <CollectionCard/>
-     <CollectionCard/>
-     <CollectionCard/>
-        </Flex>
-    )
-  }
-  
-  export default CollectionCards
+const CollectionCards = ({collections=[0,1,2,3]}:{collections:any}) => {
+  return (
+    <Flex minH={450} py={4} px={4} gap={"4"} wrap={"wrap"}>
+        {isEmpty(collections)
+        ?
+        <Box mx={'auto'} p={6}>
+<Text textAlign={'center'} fontSize={'3xl'}>You Don't have any NFTs yet</Text>
+        </Box>
+    :
+        collections.map((collection:any,i:number) => (
+            <CollectionCard collection={collection} key={'collection-'+i}/>
+            ))}
+        
+    </Flex>
+  );
+};
+
+export default CollectionCards;
